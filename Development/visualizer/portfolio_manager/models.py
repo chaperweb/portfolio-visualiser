fb2b6fca2bd6ee54955f2188370752615c437334
Miili Halkka <halkkam1@aalto.fi>
2016-12-07 13:18:30 +0200

    Setting up the database models

--- Development/visualizer/portfolio_manager/models.py
+++ Development/visualizer/portfolio_manager/models.py
@@ -6,21 +6,27 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 import reversion
 from reversion.models import Version
 
-# Create your models here.
 
+#Model for a Organization
+#Id generated automatically
 class Organization (models.Model):
   name = models.CharField(max_length=50, primary_key=True)
 
+#Model for a Project instance
+#Id generated automatically
 class Project (models.Model):
+  start_date = models.DateTimeField
   name = models.CharField(max_length=50)
   parent = models.ForeignKey("Organization", null=True)
 
+#Model for a project dimension
 class ProjectDimension (models.Model):
   project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='dimensions')
   content_type = models.ForeignKey(ContentType)
   object_id = models.PositiveIntegerField()
   dimension_object = GenericForeignKey('content_type', 'object_id')
 
+#Model for a Milestone
 class ProjectMilestone (models.Model):
   deadline = models.DateTimeField()
   project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='milestones')
@@ -32,32 +38,14 @@ class ProjectMilestone (models.Model):
 
     return True
 
+#Model for
 class ProjectMilestoneDimensionMilestone (models.Model):
   project_milestone = models.ForeignKey(ProjectMilestone, on_delete=models.CASCADE, related_name='dimension_milestones')
   content_type = models.ForeignKey(ContentType)
   object_id = models.PositiveIntegerField()
   dimension_milestone_object = GenericForeignKey('content_type', 'object_id')
-   
-class Dimension (models.Model):
-  class Meta:
-    abstract = True
 
-  name = models.CharField(max_length=64)
-  
-  def get_content_type(self):
-    return ContentType.objects.get_for_model(self).id
-
-@reversion.register()
-class NumericDimension (Dimension):
-
-  value = models.IntegerField()
-
-  def update_value(self, value, when):
-    with reversion.create_revision():
-      self.value = value
-      self.save()
-      reversion.set_date_created(when)
- 
+#model for comparisons between milestones adn NumericDimension values
 class NumericDimensionMilestone (models.Model):
   numeric_dimension = models.ForeignKey(NumericDimension, on_delete=models.CASCADE, related_name='milestones')
   value = models.IntegerField()
@@ -74,8 +62,24 @@ class NumericDimensionMilestone (models.Model):
 
     return self.value <= dimension_value
 
+#model for comparisons between milestones and DecimalDimension values
+class DecimalDimensionMilestone (models.Model):
+  numeric_dimension = models.ForeignKey(DecimalDimension, on_delete=models.CASCADE, related_name='milestones')
+  value = models.IntegerField()
+
+#model for a Dimension to use in comparisons
+class Dimension (models.Model):
+  class Meta:
+    abstract = True
+
+  name = models.CharField(max_length=64)
+
+  def get_content_type(self):
+    return ContentType.objects.get_for_model(self).id
+
 @reversion.register()
 class Person (models.Model):
+    ID = models.CharField(max_length=11)
   first_name = models.CharField(max_length=64)
   last_name = models.CharField(max_length=64)
 
@@ -89,5 +93,59 @@ class MembersDimension(Dimension):
       if (when != None):
         reversion.set_date_created(when)
 
+#Dimensions for Integer, Decimal and text inputs
+@reversion.register()
+class NumericDimension (Dimension):
 
+  value = models.IntegerField()
 
+  def update_value(self, value, when):
+    with reversion.create_revision():
+      self.value = value
+      self.save()
+      reversion.set_date_created(when)
+
+@reversion.register()
+class DecimalDimension (Dimension):
+    value = models.DecimalField(max_digits = 20, decimal_places = 2)
+
+    def update_value(self, value, when):
+        with reversion.create_revision():
+          self.value = value
+          self.save()
+          reversion.set_date_created(when)
+
+@reversion.register()
+class TextDimension (Dimension):
+    value = models.TextField()
+
+    def update_value(self, value, when):
+        with reversion.create_revision():
+          self.value = value
+          self.save()
+          reversion.set_date_created(when)
+
+ #Project size in money
+ class SizeMoney(DecimalDimension):
+
+ #Project size in man-days
+ class SizeMd(DecimalDimension):
+
+#Project size as impact
+class SizeImpact(TextDimension):
+
+#Technologies user in project
+class Technology(TextDimension):
+
+#Description of the project
+class Description(TextDimension):
+
+#Deliverer of the project
+class Deliverer(TextDimension):
+
+#Customer of the project
+class Customer(TextDimension):
+
+#Storing the project dependencies as list of project IDs
+class ProjectDependencies(Dimension):
+    value = models.ManyToManyField(Project)
