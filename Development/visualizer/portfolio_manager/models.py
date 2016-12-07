52156730d55a82b5f8c26235ddd21ad0c763fa48
Miili Halkka <halkkam1@aalto.fi>
2016-12-07 14:40:58 +0200

    All requested database models should now be there

--- Development/visualizer/portfolio_manager/models.py
+++ Development/visualizer/portfolio_manager/models.py
@@ -17,7 +17,7 @@ class Organization (models.Model):
 class Project (models.Model):
   start_date = models.DateTimeField
   name = models.CharField(max_length=50)
-  parent = models.ForeignKey("Organization", null=True)
+  parent = models.ForeignKey(Organization, null=True)
 
 #Model for a project dimension
 class ProjectDimension (models.Model):
@@ -28,6 +28,7 @@ class ProjectDimension (models.Model):
 
 #Model for a Milestone
 class ProjectMilestone (models.Model):
+  id = models.IntegerField(max_length = 64)
   deadline = models.DateTimeField()
   project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='milestones')
 
@@ -45,9 +46,9 @@ class ProjectMilestoneDimensionMilestone (models.Model):
   object_id = models.PositiveIntegerField()
   dimension_milestone_object = GenericForeignKey('content_type', 'object_id')
 
-#model for comparisons between milestones adn NumericDimension values
+#model for comparisons between milestones and NumericDimension values
 class NumericDimensionMilestone (models.Model):
-  numeric_dimension = models.ForeignKey(NumericDimension, on_delete=models.CASCADE, related_name='milestones')
+  numeric_dimension = models.ForeignKey(NumericDimension, on_delete=models.CASCADE, related_name='numeric_milestones')
   value = models.IntegerField()
 
   def on_schedule(self, deadline):
@@ -64,8 +65,15 @@ class NumericDimensionMilestone (models.Model):
 
 #model for comparisons between milestones and DecimalDimension values
 class DecimalDimensionMilestone (models.Model):
-  numeric_dimension = models.ForeignKey(DecimalDimension, on_delete=models.CASCADE, related_name='milestones')
+  numeric_dimension = models.ForeignKey(DecimalDimension, on_delete=models.CASCADE, related_name='decimal_milestones')
   value = models.IntegerField()
+
+#Model for a person
+@reversion.register()
+class Person (models.Model):
+    ID = models.CharField(max_length=11)
+  first_name = models.CharField(max_length=64)
+  last_name = models.CharField(max_length=64)
 
 #model for a Dimension to use in comparisons
 class Dimension (models.Model):
@@ -77,12 +85,7 @@ class Dimension (models.Model):
   def get_content_type(self):
     return ContentType.objects.get_for_model(self).id
 
-@reversion.register()
-class Person (models.Model):
-    ID = models.CharField(max_length=11)
-  first_name = models.CharField(max_length=64)
-  last_name = models.CharField(max_length=64)
-
+#Dimension for project participant management
 @reversion.register()
 class MembersDimension(Dimension):
   members = models.ManyToManyField(Person)
@@ -93,11 +96,19 @@ class MembersDimension(Dimension):
       if (when != None):
         reversion.set_date_created(when)
 
-#Dimensions for Integer, Decimal and text inputs
+#Dimensions for Integer, Decimal and Text inputs
+#set_value needs some minor fixes
+#update_value adds timestamp when edited
 @reversion.register()
 class NumericDimension (Dimension):
 
   value = models.IntegerField()
+
+  def set_value(self, value, when=None):
+    with reversion.create_revision():
+      self.value.set(value)
+      if (when != None):
+        reversion.set_date_created(when)
 
   def update_value(self, value, when):
     with reversion.create_revision():
@@ -109,6 +120,12 @@ class NumericDimension (Dimension):
 class DecimalDimension (Dimension):
     value = models.DecimalField(max_digits = 20, decimal_places = 2)
 
+    def set_value(self, value, when=None):
+      with reversion.create_revision():
+        self.value.set(value)
+        if (when != None):
+          reversion.set_date_created(when)
+
     def update_value(self, value, when):
         with reversion.create_revision():
           self.value = value
@@ -119,11 +136,36 @@ class DecimalDimension (Dimension):
 class TextDimension (Dimension):
     value = models.TextField()
 
+    def set_value(self, value, when=None):
+      with reversion.create_revision():
+        self.value.set(value)
+        if (when != None):
+          reversion.set_date_created(when)
+
     def update_value(self, value, when):
         with reversion.create_revision():
           self.value = value
           self.save()
           reversion.set_date_created(when)
+
+#Connection between project and a project manager
+class ProjectManager(Dimension):
+    manager = models.CharField(Person.ID)
+    project = models.IntegerField(Project)
+
+#Phase of the project as a list of Milestone IDs
+class Phase(Dimension):
+    value = models.ManyToManyField(ProjectMilestone.id)
+
+#Storing the project dependencies as list of project IDs
+class ProjectDependencies(Dimension):
+    value = models.ManyToManyField(Project)
+
+#Task class
+#Is person completing task essential?
+class Task(NumericDimension):
+    task_description = models.TextField()
+    value = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
 
  #Project size in money
  class SizeMoney(DecimalDimension):
@@ -134,8 +176,11 @@ class TextDimension (Dimension):
 #Project size as impact
 class SizeImpact(TextDimension):
 
-#Technologies user in project
+#Technologies used in project
 class Technology(TextDimension):
+
+#Development model used in project
+class DevelopmentMethod(TextDimension):
 
 #Description of the project
 class Description(TextDimension):
@@ -145,7 +190,3 @@ class Deliverer(TextDimension):
 
 #Customer of the project
 class Customer(TextDimension):
-
-#Storing the project dependencies as list of project IDs
-class ProjectDependencies(Dimension):
-    value = models.ManyToManyField(Project)
