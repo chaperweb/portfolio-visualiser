2b3d4d26cf57cb0259184d7f83426c959306c422
Flavionator <castor.kohler@aalto.fi>
2017-01-09 15:43:01 +0200

    Added being able to add organization

--- Development/visualizer/portfolio_manager/forms.py
+++ Development/visualizer/portfolio_manager/forms.py
@@ -2,18 +2,9 @@ from django import forms
 from portfolio_manager.models import Organization, Project
 
 class ProjectForm(forms.Form):
-
-
     name = forms.CharField(label = 'Name', max_length=50, required = True,
                             error_messages={'required': 'Your project needs a name!'})
     parent = forms.ModelChoiceField(queryset=Organization.objects.all())
-
-    # class Meta:
-    #     model = Project
-    #     fields = ('name', 'parent',)
-# #startTime = forms.DateTimeField(label = 'Start Date', required = True,
-    #                        error_messages={'required': 'When will the project start?'})
-    #duration = forms.IntegerField()
 
 class OrganizationForm(forms.Form):
     name = forms.CharField(label = 'Name', max_length=50, required = True,
