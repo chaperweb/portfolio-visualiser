35c6d845e907e7c9e4f0059f5bc6da026927d2be
Joonas Harjumäki <joonas.harjumaki@nsftele.com>
2017-01-09 19:46:25 +0200

    authenticate app with apikey

--- Development/visualizer/scripts/import.py
+++ Development/visualizer/scripts/import.py
@@ -3,57 +3,16 @@ from __future__ import print_function
 import httplib2
 import os
 
-from apiclient import discovery
-from oauth2client import client
-from oauth2client import tools
-from oauth2client.file import Storage
+from googleapiclient.discovery import build
+from oauth2client.client import GoogleCredentials
 
-
-# If modifying these scopes, delete your previously saved credentials
-# at ~/.credentials/sheets.googleapis.com-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
 CLIENT_SECRET_FILE = os.path.dirname(os.path.realpath(__file__))+'/client_id.json'
-APPLICATION_NAME = 'Web client 1'
-
-
-def get_credentials():
-    """Gets valid user credentials from storage.
-
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
-
-    Returns:
-        Credentials, the obtained credential.
-    """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'sheets.googleapis.com-python-quickstart.json')
-
-    store = Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        credentials = tools.run_flow(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
+API_KEY='AIzaSyDj4jirLkmyIwe42rTpdkeqY7DlCoNsIAU'
 
 def run():
-    """Shows basic usage of the Sheets API.
 
-    Creates a Sheets API service object and prints the names and majors of
-    students in a sample spreadsheet:
-    https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-    """
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
-                    'version=v4')
-    service = discovery.build('sheets', 'v4', http=http,
-                              discoveryServiceUrl=discoveryUrl)
+    credentials = GoogleCredentials.from_stream(CLIENT_SECRET_FILE)
+    service = build('sheets', 'v4', credentials=credentials, developerKey=API_KEY)
 
     spreadsheetId = '1h4qJYKxL7EhjIXh2tUghSdNAEQalc2fPwTSqHu6CrmY'
     rangeName = 'Taulukko1!A1:T'
