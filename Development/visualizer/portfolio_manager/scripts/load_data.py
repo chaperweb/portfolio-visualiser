f9858d9528493504598dfcfe8b7e0790a67346b3
Kaarlo Kekkonen <kaarlo.kekkonen@aalto.fi>
2017-01-17 23:22:34 +0200

    Obsolete script replaced

--- Development/visualizer/portfolio_manager/scripts/load_data.py
+++ Development/visualizer/portfolio_manager/scripts/load_data.py
@@ -1,34 +1,26 @@
+import gspread
 
-from __future__ import print_function
-import httplib2
-import os
+from oauth2client.service_account import ServiceAccountCredentials
 
-from googleapiclient.discovery import build
-from oauth2client.client import GoogleCredentials
 
-CLIENT_SECRET_FILE = os.path.dirname(os.path.realpath(__file__))+'/client_id.json'
-API_KEY='AIzaSyDj4jirLkmyIwe42rTpdkeqY7DlCoNsIAU'
-
-def run():
-
-    credentials = GoogleCredentials.from_stream(CLIENT_SECRET_FILE)
-    service = build('sheets', 'v4', credentials=credentials, developerKey=API_KEY)
-
-    spreadsheetId = '1h4qJYKxL7EhjIXh2tUghSdNAEQalc2fPwTSqHu6CrmY'
-    rangeName = 'Taulukko1!A1:T'
-    result = service.spreadsheets().values().get(
-        spreadsheetId=spreadsheetId, range=rangeName).execute()
-    values = result.get('values', [])
-
-    if not values:
-        print('No data found.')
-    else:
-        print('Name, Major:')
-        for row in values:
-            # Print columns A and E, which correspond to indices 0 and 4.
-            print('%s' % (row))
+			
+			
+def load_data_from_url(SheetUrl):
+	try:
+		scope = ['https://spreadsheets.google.com/feeds']
+		credentials = ServiceAccountCredentials.from_json_keyfile_name('service_account.json', scope)
+		gc = gspread.authorize(credentials)
+		Sheet = gc.open_by_url(SheetUrl)
+		worksheet = Sheet.get_worksheet(0)
+		values_list = worksheet.get_all_values()
+		return values_list
+		
+	except gspread.exceptions.SpreadsheetNotFound:
+		# url is broken or service account doesnt have rights to access the sheet
+		return [[]]
+		
 
 
 if __name__ == '__main__':
-    run()
+    load_data_from_url('https://docs.google.com/spreadsheets/d/1lNuG43KgUNDR48VxJUlNNW7fvvaQW7gRyv9Qrc7Ihy0/edit')
 
