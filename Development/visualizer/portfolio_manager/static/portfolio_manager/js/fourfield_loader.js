16f99ecb647454939d7646657c2c64c5d9d2977f
Joonas Harjumäki <joonas.harjumaki@nsftele.com>
2017-03-27 01:00:02 +0300

    parse decimal dimensions from json

--- Development/visualizer/portfolio_manager/static/portfolio_manager/js/fourfield_loader.js
+++ Development/visualizer/portfolio_manager/static/portfolio_manager/js/fourfield_loader.js
@@ -4,46 +4,28 @@ $(function() {
     url: "json"
   }).done(function(data) {
     db_json = data;
-    for (var i = 0, len = data.length; i < len; i++) {
-      $('#project-selector').append('<option value="'+data[i].id+'">'+data[i].name+'</option>')
+    
+    dimension_names = {};
+
+    for (var i = 0, ilen = db_json.length; i < ilen; i++) {
+      for(var j = 0, jlen = db_json[i].dimensions.length; j < jlen; j++) {
+        dimension = db_json[i].dimensions[j];
+        if(dimension.dimension_type == 'DecimalDimension'){
+          dimension_names[dimension.dimension_object.name] = dimension.dimension_object.name;
+        }
+       }
     }
-  });
-
-  $('#project-selector').on('change', function(){
-    project_id = $(this).find("option:selected").val();
-
-    preserved_x_name = $('#x-selector').find("option:selected").text();
-    preserved_y_name = $('#y-selector').find("option:selected").text();
-    preserved_r_name = $('#r-selector').find("option:selected").text();
 
     $('#x-selector').html('<option>---</option>');
     $('#y-selector').html('<option>---</option>');
     $('#r-selector').html('<option>---</option>');
-    for (var i = 0, len = db_json.length; i < len; i++) {
-      if(db_json[i].id == project_id) {
-        project = db_json[i];
-        for (var j = 0; j < project.dimensions.length; j++) {
-          if('history' in project.dimensions[j].dimension_object) {
 
-            x_selected = (project.dimensions[j].dimension_object.name == preserved_x_name) ? 'selected' : '';
-            y_selected = (project.dimensions[j].dimension_object.name == preserved_y_name) ? 'selected' : '';
-            r_selected = (project.dimensions[j].dimension_object.name == preserved_r_name) ? 'selected' : '';
-
-            if (project.dimensions[j].dimension_type == 'DecimalDimension') {
-              $('#x-selector').append('<option '+x_selected+' value="'+project.dimensions[j].id+'">'+project.dimensions[j].dimension_object.name+'</option>');
-            }
-            if (project.dimensions[j].dimension_type == 'DecimalDimension') {
-              $('#y-selector').append('<option '+y_selected+' value="'+project.dimensions[j].id+'">'+project.dimensions[j].dimension_object.name+'</option>');
-            }
-            if (project.dimensions[j].dimension_type == 'DecimalDimension') {
-              $('#r-selector').append('<option '+r_selected+' value="'+project.dimensions[j].id+'">'+project.dimensions[j].dimension_object.name+'</option>');
-            }
-          }
-        }
-        break;
-      }
+    for(var key in dimension_names) {
+      $('#x-selector').append('<option value="'+dimension_names[key]+'">'+dimension_names[key]+'</option>');
+      $('#y-selector').append('<option value="'+dimension_names[key]+'">'+dimension_names[key]+'</option>');
+      $('#r-selector').append('<option value="'+dimension_names[key]+'">'+dimension_names[key]+'</option>');
     }
-    dimension_selector_change();
+
   });
 
   $('#x-selector').on('change', dimension_selector_change);
