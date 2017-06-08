578f0e45814aa786884ee7e3d5eac0c414c49d77
MiiliH <miili.halkka@aalto.fi>
2017-06-08 17:54:03 +0300

    date axis date to fit into smallest container

--- Development/visualizer/portfolio_manager/static/portfolio_manager/js/path.js
+++ Development/visualizer/portfolio_manager/static/portfolio_manager/js/path.js
@@ -78,7 +78,7 @@ function generate_path_data(x_dimension, y_dimension) {
 
 function generate_path_svg(pathData) {
   // Dimension of the svg box
-  var height = Math.max(500, $(window).height()*0.7),
+  var height = Math.max(600, $(window).height()*0.7),
       width = Math.max(700, ($(window).width()-250)*0.8),
       margin = {
         right: 0,
