95e1c1866a14254b953a36d81dc0ad24f8b66b91
MiiliH <miili.halkka@aalto.fi>
2017-06-08 16:44:15 +0300

    redefining width parameters for visualization and the legend

--- Development/visualizer/portfolio_manager/static/portfolio_manager/js/projectDependencies.js
+++ Development/visualizer/portfolio_manager/static/portfolio_manager/js/projectDependencies.js
@@ -139,8 +139,8 @@ function dependancies(json) {
 		});
 	}
 	// size of the display box and definition of colorscale (predefined ATM)
-	var height = $(window).height()*0.85,
-		width = height,
+	var height = Math.max(500,$(window).height()*0.85),
+		width = Math.max(700, $(window).width()*0.5),
 		color = d3.scale.category20();
 
  // Ball outline is 3 pixels wide
@@ -319,11 +319,12 @@ function dependancies(json) {
 		return x;
 	}
 
-	/* The legend below the graph is given its own svg container in
+	/* The legend nesx to the graph is given its own svg container in
    * which we have the project color, name and budget respectively.
    */
     var legendSpacing = 4;
     var legendRectSize = 25;
+    var legendWidth = Math.max(300,width*0.5)
     var legendHeight = legendRectSize + legendSpacing;
 
     var svgLegend = d3.select("body")
@@ -354,6 +355,5 @@ function dependancies(json) {
           .data(nodeValueArray)
           .attr("x", legendRectSize + legendSpacing)
           .attr("y", legendRectSize - legendSpacing)
-          .style("font", "16px sans-serif")
           .text(function(d){return ""+d+"€"});
 }
