1f878110c4e1a18abbc992e4eddc4a364c0d50f5
Tuukka Tolvanen <tuukka.tolvanen@codento.com>
2017-06-21 16:33:19 +0300

    Unbuffer python so docker test progress is visible

--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -3,5 +3,6 @@
 cd /app
 source venv/bin/activate
 PATH=/app/node_modules/.bin:$PATH
+export PYTHONUNBUFFERED=1
 python manage.py test --settings visualizer.test_settings
 npm test | tap-xunit > junit/js-test.xml
