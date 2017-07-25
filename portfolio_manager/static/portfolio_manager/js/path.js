9f38e3c1c889e12d954a3b9130bed6fccdd67c94
MiiliH <miili.halkka@aalto.fi>
2017-07-25 17:22:16 +0300

    typofix

--- portfolio_manager/static/portfolio_manager/js/path.js
+++ portfolio_manager/static/portfolio_manager/js/path.js
@@ -265,7 +265,7 @@ function generate_path_svg(target, data_id_array) {
         .attr("id", rounds)
         .attr("d", d3.area().x(function(d) {return xScale(d.history_date)})
                             .y0(function(d) {return rounds * xAxesHeight + 2})
-                            .y1(function(d) {return rounds * xAxesHeight + xAxesHeight - 1}));
+                            .y1(function(d) {return rounds * xAxesHeight + xAxesHeight - 1}))
                             .on("mouseover", function(d){ div.style("opacity", .7); div.html(d[bisectX(d, d3.event.offsetX)].value)
                                         .style("left", this.getScreenCTM().e + this.getBBox().x + "px")
                                         .style("top", this.getScreenCTM().f + this.getBBox().y + "px");})
