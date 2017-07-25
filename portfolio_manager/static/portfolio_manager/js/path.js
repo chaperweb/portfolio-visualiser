7750e4758df856b726a965f5d9981b8e5c272c51
MiiliH <miili.halkka@aalto.fi>
2017-07-25 17:29:12 +0300

    div x-coordinate rewind

--- portfolio_manager/static/portfolio_manager/js/path.js
+++ portfolio_manager/static/portfolio_manager/js/path.js
@@ -269,7 +269,7 @@ function generate_path_svg(target, data_id_array) {
                             .y0(function(d) {return rounds * xAxesHeight + 2})
                             .y1(function(d) {return rounds * xAxesHeight + xAxesHeight - 1}))
                             .on("mouseover", function(d){ div.style("opacity", .7); div.html(d[bisectX(d, d3.event.offsetX)].value)
-                                        .style("left", this.getScreenCTM().e + d[bisectX(d, d3.event.offsetX)].x + "px")
+                                        .style("left", this.getScreenCTM().e + this.getBBox().x + "px")
                                         .style("top", this.getScreenCTM().f + this.getBBox().y + "px");})
                             .on("mouseout", function(){return div.style("opacity", 0);});
 
