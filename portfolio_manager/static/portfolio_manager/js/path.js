a2c2a4c80331fd6833587e86467d5770212799c6
MiiliH <miili.halkka@aalto.fi>
2017-07-25 17:57:29 +0300

    xScaling values

--- portfolio_manager/static/portfolio_manager/js/path.js
+++ portfolio_manager/static/portfolio_manager/js/path.js
@@ -268,9 +268,10 @@ function generate_path_svg(target, data_id_array) {
         .attr("d", d3.area().x(function(d) {return xScale(d.history_date)})
                             .y0(function(d) {return rounds * xAxesHeight + 2})
                             .y1(function(d) {return rounds * xAxesHeight + xAxesHeight - 1}))
-                            .on("mouseover", function(d){ div.style("opacity", .7); div.html(d[bisectX(d, d3.event.offsetX)].value)
-                                        .style("left", this.getScreenCTM().e + xScale(d[bisectX(d, d3.event.offsetX)].history_date) + "px")
-                                        .style("top", this.getScreenCTM().f + this.getBBox().y + "px");})
+                            .on("mouseover", function(d){ div.style("opacity", .7);
+                                                          div.html(d[bisectX(d, xScale.domain(d3.event.offsetX))].value)
+                                                             .style("left", this.getScreenCTM().e + xScale(d[bisectX(d, xScale.domain(d3.event.offsetX))].history_date) + "px")
+                                                             .style("top", this.getScreenCTM().f + this.getBBox().y + "px");})
                             .on("mouseout", function(){return div.style("opacity", 0);});
 
     /*
