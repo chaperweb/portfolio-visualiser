279c8f3ba6e5f5d2bc180cc315add7d16d399bc7
CastorK <castor.kohler@aalto.fi>
2017-07-25 14:38:41 +0300

    Add animation add base interaction for addrowbtn

--- portfolio_manager/static/portfolio_manager/js/manage/milestone.js
+++ portfolio_manager/static/portfolio_manager/js/manage/milestone.js
@@ -1,9 +1,32 @@
+$.fn.refresh = function() {
+    return $(this.selector);
+};
+
+function addClick(btn){
+  tbody = $(btn).data('pid') + "-tablebody";
+  due_date = "<td><input class='text-center' type='date'/></td>";
+  td = "<td><input class='text-center' type='number'/></td>";
+  tr = "<tr><form>" + due_date + td + td + td + "</form></tr>";
+  $(tr).appendTo("#" + tbody);
+
+  $(btn).toggleClass('submit');
+  $('.icons').toggleClass('icons-active');
+}
+
+function submitClick(btn) {
+  alert("THIS SHOULD SUBMIT");
+
+  $(btn).toggleClass('submit');
+  $('.icons').toggleClass('icons-active');
+}
+
 $(function(){
-  $(".btn").click(function(){
-    tbody = $(this).data('pid') + "-tablebody";
-    due_date = "<td><input class='text-center' type='date'/></td>"
-    td = "<td><input class='text-center' type='number'/></td>";
-    tr = "<tr><form>" + due_date + td + td + td + "</form></tr>";
-    $(tr).appendTo("#" + tbody);
+  $(".add-row-btn").click(function(){
+    if (!$(this).hasClass('submit')) {
+      addClick(this);
+    }
+    else {
+      submitClick(this);
+    }
   });
 });
