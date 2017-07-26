54fd184f4b0d77b5bf9d8a9dbe4c8cb00956b809
MiiliH <miili.halkka@aalto.fi>
2017-07-26 11:10:03 +0300

    debug prints

--- portfolio_manager/static/portfolio_manager/js/path.js
+++ portfolio_manager/static/portfolio_manager/js/path.js
@@ -268,7 +268,7 @@ function generate_path_svg(target, data_id_array) {
         .attr("d", d3.area().x(function(d) {return xScale(d.history_date)})
                             .y0(function(d) {return rounds * xAxesHeight + 2})
                             .y1(function(d) {return rounds * xAxesHeight + xAxesHeight - 1}))
-                            .on("mouseover", function(d){ console.log(d, xScale(d3.event.offsetX))
+                            .on("mouseover", function(d){ console.log(d, d3.event.offsetX)
                                                           div.style("opacity", .7);
                                                           div.html(d[bisectX(d, xScale(d3.event.offsetX))].value)
                                                              .style("left", this.getScreenCTM().e + xScale(d[bisectX(d, xScale(d3.event.offsetX))].history_date) + "px")
