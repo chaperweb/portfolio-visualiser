c07e329d5bce81a9eca92f20e6de8065671f14f2
MiiliH <miili.halkka@aalto.fi>
2017-09-08 13:12:36 +0300

    missing , in urls

--- portfolio_manager/urls.py
+++ portfolio_manager/urls.py
@@ -70,7 +70,7 @@ urlpatterns = [
     url(r'^presentations/(?P<presentation_id>[0-9]+)$', views.presentation, name="presentations"),
     url(r'^presentations/edit_presentation/(?P<presentation_id>[0-9]+)$', views.save_presentation, name="edit_presentation"),
     url(r'^presentations/new_presentation$', TemplateView.as_view(template_name="presentations/new_presentation.html"), name='new_presentation'),
-    url(r'^presentations/save_presentation$', views.save_presentation, name='save_presentation')
+    url(r'^presentations/save_presentation$', views.save_presentation, name='save_presentation'),
     # these are for ajax requests
     url(r'', include(ajax_patterns)),
 
