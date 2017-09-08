6bd90ed92a4ace84edb7a3a5368ca9d0e6efc98d
MiiliH <miili.halkka@aalto.fi>
2017-09-08 18:12:09 +0300

    edit presentation page

--- portfolio_manager/views.py
+++ portfolio_manager/views.py
@@ -962,7 +962,6 @@ def create_snapshot(request):
 
 @login_required
 @user_passes_test(is_admin)
-@require_POST
 def save_presentation(request, presentation_id = None):
 
     if not presentation_id:
@@ -988,6 +987,7 @@ def save_presentation(request, presentation_id = None):
             summary = presentation.summary
 
             response_data = {
+            'id': presentation.pk,
             'title': title,
             'summary': summary#,
             #'snapshots': presentation.snapshots
