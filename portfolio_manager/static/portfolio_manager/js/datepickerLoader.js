b5f396ad24d4e1a8e157015d3852c135a9a69d44
Kaarlo Kekkonen <kaarlo.kekkonen@codento.com>
2017-11-27 15:53:36 +0200

    Added static scripts to set JQuery datepicker on datefields when the user is using firefox or IE browser.

---
+++ portfolio_manager/static/portfolio_manager/js/datepickerLoader.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+  //test if user is on firefox or IE, if so, JQuery datepicker is used on edit-project datefields
+  var isFirefox = typeof InstallTrigger !== 'undefined';
+  var isIE = /*@cc_on!@*/false || !!document.documentMode;
+  if(isFirefox || isIE) {
+    $("#date-value").datepicker();
+  };
+};
